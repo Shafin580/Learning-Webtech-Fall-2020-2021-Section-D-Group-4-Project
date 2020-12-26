530fac7a09e975b205eab78208e50959e1b08ba5
Shafin580 <johndoe@example.com>
2020-12-27 00:33:11 +0600

    clientScript.js updated

--- assets/scripts/clientScript.js
+++ assets/scripts/clientScript.js
@@ -25,42 +25,6 @@ function productInfo(){
     
 }
 
-function userValidate(){
-    
-    var submit = document.querySelector("#Submit").value;
-    
-    var username = document.querySelector("#userName").value;
-    
-    var password = document.querySelector("#Password").value;
-    
-    if(username === "" || password === ""){
-        
-        document.querySelector("#response").innerHTML = "Missing Information";
-        
-    }else{
-            
-    var xhttp = new XMLHttpRequest();
-	xhttp.open('POST', 'php/loginValidate.php', true);
-	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-	xhttp.onreadystatechange = function(){
-		
-		if (this.readyState == 4 && this.status == 200) {
-            
-            if(this.responseText=="Valid"){
-                
-                window.location.href = "view/client_home.php";
-                
-            }else{document.querySelector("#response").innerHTML = this.responseText;}
-            
-	    	
-	    }
-	}
-	
-	xhttp.send('submit='+submit+'&username='+username+'&password='+password);
-        
-    }
-    
-}
 
 
